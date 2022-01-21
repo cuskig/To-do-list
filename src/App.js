43c9885626010095a47786bdff8264f30b05d76a
cuskig <cuskiggg@gmail.com>
2022-01-21 12:06:36 +0000

    Done it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
+  let [item, setitem] = React.useState('');
+  let [todos, settodos] = React.useState([]);
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <input value={item} onChange={(e) => setitem(e.target.value)} />
+      <button
+        onClick={() => {
+          settodos(todos.concat(item));
+          setitem('');
+        }}
+      >
+        Add
+      </button>
+      <div>
+        {todos.map((t, i) => (
+          <div>
+            <input
+              type="checkbox"
+              checked={false}
+              onChange={() => {
+                let newtodos = todos.slice()
+                newtodos.splice(i, 1)
+                settodos(newtodos);
+              }}
+            />
+            {t}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
